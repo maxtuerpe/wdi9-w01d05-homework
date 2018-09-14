e3b9653624ea6d5212122b1aa35db8f729810ee4
Max Tuerpe <maxtuerpe4@gmail.com>
2018-09-14 15:17:38 -0600

    1.B

---
+++ app.js
@@ -0,0 +1,17 @@
+// variable = value
+//variable = newValue
+// const/let newVariable = variable
+// declaring is naming a variable. defining is giving a variable value. assigning is updating the value.
+// pseudocoding is writing out what steps you think you need your code to do.
+// 80-90% of time should be spent thnking abou how code should work.
+
+let firstVariable = 'Hello World';
+firstVariable = 4;
+secondVariable = firstVariable;
+secondVariable = 'max';
+// firstVariable is still defined as 4.
+
+
+return (`Hello, my name is ${secondVariable}`);
+
+
