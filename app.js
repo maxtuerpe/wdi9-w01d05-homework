0b62e99ebf702a4c1bfd6dc67ec5b20cf407b729
Max Tuerpe <maxtuerpe4@gmail.com>
2018-09-14 15:29:18 -0600

    1.C

--- app.js
+++ app.js
@@ -10,8 +10,23 @@ firstVariable = 4;
 secondVariable = firstVariable;
 secondVariable = 'max';
 // firstVariable is still defined as 4.
+console.log(`Hello, my name is ${secondVariable}`);
 
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
 
-return (`Hello, my name is ${secondVariable}`);
+console.log(a < b);
+console.log(c > d);
+console.log('Name' === 'Name');
+console.log(true != false);
+console.log(false == false == false == false == false == false == true);
+console.log(false == false)
+console.log(e === 'Kevin');
+console.log(a < b < c);
+console.log(a == a !== d);
+console.log(48 == '48');
 
 
