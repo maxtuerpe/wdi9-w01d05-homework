159ca87d64284a5d90427811d901a75291ff1c28
Max Tuerpe <maxtuerpe4@gmail.com>
2018-09-14 15:34:23 -0600

    1.D

--- app.js
+++ app.js
@@ -18,15 +18,23 @@ const c = 57;
 const d = 16;
 const e = 'Kevin';
 
-console.log(a < b);
-console.log(c > d);
-console.log('Name' === 'Name');
-console.log(true != false);
-console.log(false == false == false == false == false == false == true);
-console.log(false == false)
-console.log(e === 'Kevin');
-console.log(a < b < c);
-console.log(a == a !== d);
-console.log(48 == '48');
+// console.log(a < b);
+// console.log(c > d);
+// console.log('Name' === 'Name');
+// console.log(true != false);
+// console.log(false == false == false == false == false == false == true);
+// console.log(false == false)
+// console.log(e === 'Kevin');
+// console.log(a < b < c);
+// console.log(a == a !== d);
+// console.log(48 == '48');
+
+
+let animal = "monkey";
+if(animal === "cow"){
+    console.log('moooo')
+} else {
+    console.log('Hey! You\'re not a cow.')
+}
 
 
